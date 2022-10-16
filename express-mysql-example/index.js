afb72b0ac5913b3e0b489561cce3a2881c64610f
aajy <aajy.000@gmail.com>
2022-10-17 01:43:15 +0900

    Node.js와 MySQL 연동 npm start 로 node index.js실행하기

--- express-mysql-example/index.js
+++ express-mysql-example/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'Aajy8094!@',
+    database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM Users', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('Users info is: ', rows);
+});
+
+connection.end();
+
