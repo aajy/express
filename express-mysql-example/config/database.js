8279f9f10d6b20ae926425780bc755e30f64ce76
jyan <jy.an@fxgear.net>
2022-10-19 19:08:21 +0900

    study1 CRUD sql문 짜기 시작

--- express-mysql-example/config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host : 'localhost',
-    user : 'root',
-    password : 'Aajy8094!@',
-    database : 'my_db'
-};
--- express-mysql-example/index.js
+++ express-mysql-example/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
-const dbconfig = require('./config/database.js');
+const { password } = require('./library/database.js');
+const dbconfig = require('./library/database.js');
 const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 
+connection.connect();
 //configuration ==================
 app.set('port',process.env.PORT ||3000);
 
@@ -13,14 +15,32 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/users', (req,res)=> {
-    connection.query('SELECT * from Users', (error, rows)=> {
-        if (error) throw error;
+    connection.query('SELECT * from users', (error, rows)=> {
         console.log('User database is ', rows);
         res.send(rows);
     });
 });
 
+app.get('/api/posts/:year/:month',(req, res) => {
+    res.send(req.params);
+})
+
+const courses = [
+    { id: 'aaa', password: '1111'},
+    { id: 'bbb', password: '2222'},
+    { id: 'ccc', password: '3333'},
+];
+const sql = ''
+
+app.get("/api/courses", (req, res) => {
+    connection.query(sql, (error, rows)=> {
+        res.send(courses);
+    });
+    connection.end();
+})
+
 app.listen(app.get('port'), () => {
     console.log('Express server listening on port '+ app.get('port'));
 });
+
 
