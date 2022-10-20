699d7984f2d8784f4f31f66dcb700747d6b5c7be
jyan <jy.an@fxgear.net>
2022-10-20 18:38:49 +0900

    localhost:3000/users CRUD API테스트, 다음으로 connection 모듈화하기

--- express-mysql-example/index.js
+++ express-mysql-example/index.js
@@ -1,56 +1,111 @@
 const express = require('express');
 const mysql = require('mysql');
-const dbconfig = require('./library/database.js');
-const connection = mysql.createConnection(dbconfig);
+const dbconfig = require('./config/config.js');
+
+// const connect = require('./module/connection.js');
 
 const app = express();
 
-// connection.connect();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //configuration ==================
 app.set('port',process.env.PORT ||3000);
 
 app.get('/', (req, res)=>{
-    res.send('Root');
+  res.send('Root');
 });
+
 // 조회
 app.get('/users', (req,res)=> {
+  const connection = mysql.createConnection(dbconfig);
+  connection.connect(function(error) {
+    if(!error) {
+      console.log('Database is connected ... nn');
+    } else {
+      console.log('Error connecting databases ... nn' + error);
+    }
+
     connection.query('SELECT * from users', (error, rows)=> {
-        console.log('User database is ', rows);
-        res.send(rows);
+      if(error) console.log(error);
+      res.send(rows);
+      connection.end();
     });
+  });
 });
 
 // 생성
-app.post("/users/create", (req, res) => {
-    const insertSql = `INSERT INTO users(id, password, age) VALUES('aaa','1111',20)`
-    console.log('req',req.params);
-    connection.query(insertSql, (error, rows)=> {
-        if(error) console.log(error);
-        res.send(rows);
+app.post("/users", (req, res) => {
+    const connection = mysql.createConnection(dbconfig);
+    connection.connect(function(error) {
+        if(!error) {
+            console.log('Database is connected ... nn');
+        } else {
+            console.log('Error connecting databases ... nn' + error);
+        }
+
+        const insertSql = `INSERT INTO users (id, password, age) VALUES ("${req.body.id}","${req.body.password}",${req.body.age? req.body.age : null})`
+        connection.query(insertSql, (error, rows)=> {
+            if(error) console.log(error);
+            res.send(rows);
+            connection.end();
+        });
     });
-    // connection.end();
 });
 
 // 수정
-app.put("/users/update", (req, res) => {
-    const insertSql = `UPDATE users(id, password, age) VALUES('aaa','1111',20)`
-    console.log('req',req.params);
+app.put("/users/:seq", (req, res) => {
+  let seq = req.params.seq;
+  let _strBody = JSON.stringify(req.body)
+  _strBody = _strBody.replace(/:/gi, '=');
+  _strBody = _strBody.replace(/{|}/gi, '');
+
+  let strBody = '';
+  let _tempArr = _strBody.split(',');
+  for (let item of _tempArr) {
+    strBody += item.split("=")[0].replace(/"/gi, '');
+    strBody += "=";
+    strBody += item.split("=")[1];
+    strBody += ',';
+  }
+
+  strBody = strBody.slice(0, strBody.length -1);
+  console.log('strBody: ', strBody);
+  const connection = mysql.createConnection(dbconfig);
+  connection.connect(function(error) {
+    if(!error) {
+      console.log('Database is connected ... nn');
+    } else {
+      console.log('Error connecting databases ... nn' + error);
+    }
+
+    const insertSql = `UPDATE users SET ${strBody} WHERE seq = ${seq};`
     connection.query(insertSql, (error, rows)=> {
-        if(error) console.log(error);
-        res.send(rows);
+      if(error) console.log(error);
+      res.send();
+      connection.end();
     });
-    // connection.end();
+  });
 });
 
 // 삭제
-app.delete("/users/delete", (req, res) => {
-    const insertSql = `INSERT INTO users(id, password, age) VALUES('aaa','1111',20)`
-    console.log('req',req.params);
-    connection.query(insertSql, (error, rows)=> {
-        if(error) console.log(error);
-        res.send(rows);
+app.delete("/users/:seq", (req, res) => {
+  let seq = req.params.seq;
+    const connection = mysql.createConnection(dbconfig);
+    connection.connect(function(error) {
+        if(!error) {
+            console.log('Database is connected ... nn');
+        } else {
+            console.log('Error connecting databases ... nn' + error);
+        }
+
+        const insertSql = `DELETE FROM users WHERE seq = ${seq}`
+        connection.query(insertSql, (error, rows)=> {
+            if(error) console.log(error);
+            res.send(rows);
+            connection.end();
+        });
     });
-    // connection.end();
 });
 
 
@@ -58,4 +113,4 @@ app.listen(app.get('port'), () => {
     console.log('Express server listening on port '+ app.get('port'));
 });
 
-module.exports = connection;
+// module.exports = connection;
